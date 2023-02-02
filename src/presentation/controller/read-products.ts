2a240e2335245c4f47988f25c6d7dd8312648307
Douglinhas <daguiaralcantara@gmail.com>
2023-02-02 17:00:08 -0300

    git commit -m "fix: change import of HttpRequest"

--- src/presentation/controller/read-products.ts
+++ src/presentation/controller/read-products.ts
@@ -1,12 +1,12 @@
 import { ReadProducts } from "../../domain/useCases/read-products";
 import { serverError } from "../helpers/http-helper";
 import { Controller } from "../protocols/controller";
-import { HttpResquest, HttpResponse } from "../protocols/http";
+import { HttpRequest, HttpResponse } from "../protocols/http";
 
 export class ReadProductsController implements Controller {
   constructor(private readProducts: ReadProducts) {}
 
-  async handle(_httpResquest: HttpResquest): Promise<HttpResponse> {
+  async handle(_httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
       const products = await this.readProducts.read();
 
--- src/presentation/controller/read-users.ts
+++ src/presentation/controller/read-users.ts
@@ -1,12 +1,12 @@
 import { ReadUsers } from "../../domain/useCases/read-users";
 import { serverError } from "../helpers/http-helper";
 import { Controller } from "../protocols/controller";
-import { HttpResquest, HttpResponse } from "../protocols/http";
+import { HttpRequest, HttpResponse } from "../protocols/http";
 
 export class ReadUsersController implements Controller {
   constructor(private readUsers: ReadUsers) {}
 
-  async handle(_httpResquest: HttpResquest): Promise<HttpResponse> {
+  async handle(_httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
       const users = await this.readUsers.read();
 

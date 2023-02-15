db144821e2d996dea6eb6b9d1da4ca1a57bf01ad
Douglinhas <daguiaralcantara@gmail.com>
2023-02-15 12:19:39 -0300

    refactor: change mockendGet implemantition

--- src/utils/mockend-utils.spec.ts
+++ src/utils/mockend-utils.spec.ts
@@ -4,8 +4,8 @@ const PRODUCTS_URL = "https://mockend.com/juunegreiros/BE-test-api/products";
 const USERS_URL = "https://mockend.com/juunegreiros/BE-test-api/users";
 
 describe("Mockend Utils", () => {
-  it("Should return an array of Products when receive the product url", async () => {
-    const products = await mockendGet(PRODUCTS_URL);
+  it("Should return an array of Products when receive the products value", async () => {
+    const products = await mockendGet("products");
 
     expect(products).toBeTruthy();
     expect(products[0]).toHaveProperty("id");
@@ -13,8 +13,8 @@ describe("Mockend Utils", () => {
     expect(products[0]).toHaveProperty("price");
   });
 
-  it("Should return an array of Users when receive the user url", async () => {
-    const users = await mockendGet(USERS_URL);
+  it("Should return an array of Users when receive the users value", async () => {
+    const users = await mockendGet("users");
 
     expect(users).toBeTruthy();
     expect(users[0]).toHaveProperty("id");
--- src/utils/mockend-utils.ts
+++ src/utils/mockend-utils.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { Product } from "../domain/models/product";
 import { User } from "../domain/models/user";
 
-export const mockendGet = async (url: string): Promise<User[] | Product[]> => {
-  const response = await axios.get(url)
+export const mockendGet = async (type: "users" | "products"): Promise<User[] | Product[]> => {
+  const response = await axios.get(`https://mockend.com/juunegreiros/BE-test-api/${type}`)
   return response.data;
 }

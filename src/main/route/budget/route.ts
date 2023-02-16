70c4b6f64bba8eeec19c8e778614fc55e5046ace
Douglinhas <daguiaralcantara@gmail.com>
2023-02-16 11:43:05 -0300

    feat: create users route and test

--- src/main/route/budget/route.ts
+++ src/main/route/budget/route.ts
@@ -1,6 +1,6 @@
 import { Router } from "express"
-import { expressAdapter } from "../adapter/express-adapter"
-import { makeCreateBudgetController } from '../factories/create-budget';
+import { expressAdapter } from "../../adapter/express-adapter"
+import { makeCreateBudgetController } from '../../factories/create-budget';
 
 export default (router: Router): void => {
   router.post("/budget/", expressAdapter(makeCreateBudgetController()));

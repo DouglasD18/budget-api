b06a6c4854807186953aec178ea18067f6a0f37f
Douglinhas <daguiaralcantara@gmail.com>
2023-02-16 11:57:43 -0300

    feat: create products route and test

--- src/main/config/router.ts
+++ src/main/config/router.ts
@@ -1,10 +1,12 @@
 import { Router, Express } from "express";
 import budgetRouter from "../route/budget/route";
 import usersRouter from "../route/users/route";
+import productsRouter from "../route/products/route";
 
 export default (app: Express): void => {
   const router = Router();
   app.use("/api", router);
   budgetRouter(router);
   usersRouter(router);
+  productsRouter(router);
 }

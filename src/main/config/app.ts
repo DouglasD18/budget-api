3e20ee537fd9f6c669864390de056b29ae0c7e5f
Douglinhas <daguiaralcantara@gmail.com>
2023-02-15 21:03:34 -0300

    feat: add swagger docs.

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,8 +1,10 @@
 import express from "express";
+import setupSwagger from "./swagger";
 import setupMiddlewares from "./middlewares";
 import setupRouter from "./router";
 
 const app = express();
+setupSwagger(app);
 setupMiddlewares(app);
 setupRouter(app);
 
